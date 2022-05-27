2145bb71f742f38bfdb092c5e4ace90ac3a89f3e
Joseph Witten <51753861+iamSm9l@users.noreply.github.com>
2022-05-27 20:46:56 +0100

    Add files via upload

--- passiveEnum.sh
+++ passiveEnum.sh
@@ -14,32 +14,59 @@ fi
 
 if [ ${#domain} -gt 0 ] 
 then    
-    curl -s "https://crt.sh/\\?q\\=\\%.$domain\\&output\\=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u
-    curl "https://subbuster.cyberxplore.com/api/find?domain=$domain" -s | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+"
+    crtsh.py $domain
+    curl -s "https://subbuster.cyberxplore.com/api/find?domain=$domain" -s | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+"
     curl -s "https://riddler.io/search/exportcsv?q=pld:$domain" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u
-    curl -s "https://certspotter.com/api/v1/issuances?domain=$domain&include_subdomains=true&expand=dns_names" | jq .[].dns_names | tr -d '[]"\n ' | tr ',' '\n'
-    curl "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq -Mr '.passive_dns | .[] | .hostname'
-    curl "https://api.hackertarget.com/hostsearch/?q=$domain" | cut -d, -f1
-    curl "https://rapiddns.io/subdomain/$domain" | grep "$domain" | grep -v "=\|RapidDNS" | sed -e "s/^<td>//" | sed -e "s/<\/td>//"
-    curl "https://api.threatminer.org/v2/domain.php?q=$domain\&rt=5" | jq -Mr '.results | .[]'
-    curl "https://threatcrowd.org/searchApi/v2/domain/report/?domain=$domain" | jq -Mr '.subdomains | .[]'
-    curl "https://urlscan.io/api/v1/search/?q=$domain" | jq -Mr '.results | .[] | .task.domain' | grep "$domain"
-    curl "https://sonar.omnisint.io/subdomains/$domain" | jq -Mr .[]
+    curl -s "https://certspotter.com/api/v1/issuances?domain=$domain\&include_subdomains=true\&expand=dns_names" |  jq -Mr '.[].dns_names | .[]' | sort -u
+    curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq -Mr '.passive_dns | .[] | .hostname'
+    curl -s "https://api.hackertarget.com/hostsearch/?q=$domain" | cut -d, -f1
+    curl -s "https://rapiddns.io/subdomain/$domain" | grep "$domain" | grep -v "=\|RapidDNS" | sed -e "s/^<td>//" | sed -e "s/<\/td>//"
+    curl -s "https://api.threatminer.org/v2/domain.php?q=$domain&rt=5" | jq -Mr '.results | .[]'
+    curl -s "https://threatcrowd.org/searchApi/v2/domain/report/?domain=$domain" | jq -Mr '.subdomains | .[]'
+    curl -s "https://urlscan.io/api/v1/search/?q=$domain" | jq -Mr '.results | .[] | .task.domain' | grep "$domain"
+    curl -s "https://sonar.omnisint.io/subdomains/$domain" | jq -Mr .[]
 fi
 
 if [ ${#filename} -gt 0 ]
 then
     while read root; do
-        curl -s "https://crt.sh/\\?q\\=\\%.$root\\&output\\=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u
-        curl "https://subbuster.cyberxplore.com/api/find?domain=$root" -s | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+"
-        curl -s "https://riddler.io/search/exportcsv?q=pld:$root" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u
-        curl -s "https://certspotter.com/api/v1/issuances?domain=$root&include_subdomains=true&expand=dns_names" | jq .[].dns_names | tr -d '[]"\n ' | tr ',' '\n'
-        curl "https://otx.alienvault.com/api/v1/indicators/domain/$root/passive_dns" | jq -Mr '.passive_dns | .[] | .hostname'
-        curl "https://api.hackertarget.com/hostsearch/?q=$root" | cut -d, -f1
-        curl "https://rapiddns.io/subdomain/$root" | grep "$root" | grep -v "=\|RapidDNS" | sed -e "s/^<td>//" | sed -e "s/<\/td>//"
-        curl "https://api.threatminer.org/v2/domain.php?q=$root\&rt=5" | jq -Mr '.results | .[]'
-        curl "https://threatcrowd.org/searchApi/v2/domain/report/?domain=$root" | jq -Mr '.subdomains | .[]'
-        curl "https://urlscan.io/api/v1/search/?q=$root" | jq -Mr '.results | .[] | .task.domain' | grep "$root"
-        curl "https://sonar.omnisint.io/subdomains/$root" | jq -Mr .[]
+        echo "$domain"
+        crtsh.py $domain
+        curl -s "https://crt.sh/?q=%.$domain\&output=json" | jq -Mr '.[] | .name_value'
+        #curl -s "https://subbuster.cyberxplore.com/api/find?domain=$domain" -s | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+"
+        curl -s "https://riddler.io/search/exportcsv?q=pld:$domain" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u
+        #curl -s "https://certspotter.com/api/v1/issuances?domain=$domain\&include_subdomains=true\&expand=dns_names" |  jq -Mr '.[].dns_names | .[]' | sort -u
+        curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq -Mr '.passive_dns | .[] | .hostname'
+        #curl -s "https://api.hackertarget.com/hostsearch/?q=$domain" | cut -d, -f1
+        curl -s "https://rapiddns.io/subdomain/$domain" | grep "$domain" | grep -v "=\|RapidDNS" | sed -e "s/^<td>//" | sed -e "s/<\/td>//"
+        curl -s "https://api.threatminer.org/v2/domain.php?q=$domain&rt=5" | jq -Mr '.results | .[]'
+        curl -s "https://threatcrowd.org/searchApi/v2/domain/report/?domain=$domain" | jq -Mr '.subdomains | .[]'
+        curl -s "https://urlscan.io/api/v1/search/?q=$domain" | jq -Mr '.results | .[] | .task.domain' | grep "$domain"
+        curl -s "https://sonar.omnisint.io/subdomains/$domain" | jq -Mr .[]
     done <$filename
 fi
+
+: <<'END_COMMENT
+crtsh.py $domain | anew allSubs.txt
+curl -s "https://riddler.io/search/exportcsv?q=pld:$domain" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | anew allSubs.txt 
+curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq -Mr '.passive_dns | .[] | .hostname' | anew allSubs.txt 
+curl -s "https://rapiddns.io/subdomain/$domain" | grep "$domain" | grep -v "=\|RapidDNS" | sed -e "s/^<td>//" | sed -e "s/<\/td>//" | grep -v "<" | anew allSubs.txt
+curl -s "https://rapiddns.io/subdomain/$domain" | grep "$domain" | grep -v "=\|RapidDNS" | sed -e "s/^<td>//" | sed -e "s/<\/td>//" | grep -v "<" | anew allSubs.txt
+curl -s "https://urlscan.io/api/v1/search/?q=$domain" | jq -Mr '.results | .[] | .task.domain' | grep "$domain" | anew allSubs.txt
+curl -s "https://sonar.omnisint.io/subdomains/$domain" | jq -Mr .[] | grep -v "no results found" | anew allSubs.txt
+#API NEEDED/LIMITED/SLOW
+curl -s "https://subbuster.cyberxplore.com/api/find?domain=$domain" -s | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | anew allSubs.txt
+curl -s "https://certspotter.com/api/v1/issuances?domain=$domain&include_subdomains=true&expand=dns_names" |  jq -Mr '.[].dns_names | .[]' | sort -u | grep -v "\*" | anew allSubs.txt
+curl -s "https://api.hackertarget.com/hostsearch/?q=$domain" | cut -d, -f1 | anew allSubs.txt
+curl -s "https://api.threatminer.org/v2/domain.php?q=$domain&rt=5" | jq -Mr '.results | .[]' | anew allSubs.txt 
+
+
+END_COMMENT
+
+while read domain; do crtsh.py $domain | anew allSubs.txt; done <rootdomains.txt
+while read domain; do curl -s "https://riddler.io/search/exportcsv?q=pld:$domain" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | anew allSubs.txt ; done <rootdomains.txt
+while read domain; do curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq -Mr '.passive_dns | .[] | .hostname' | anew allSubs.txt ; done <rootdomains.txt
+while read domain; do curl -s "https://rapiddns.io/subdomain/$domain" | grep "$domain" | grep -v "=\|RapidDNS" | sed -e "s/^<td>//" | sed -e "s/<\/td>//" | anew allSubs.txt; done <rootdomains.txt
+while read domain; do curl -s "https://threatcrowd.org/searchApi/v2/domain/report/?domain=$domain" | jq -Mr '.subdomains | .[]' | anew allSubs.txt; done <rootdomains.txt
+while read domain; do curl -s "https://urlscan.io/api/v1/search/?q=$domain" | jq -Mr '.results | .[] | .task.domain' | grep "$domain" | anew allSubs.txt; done <rootdomains.txt
+while read domain; do curl -s "https://sonar.omnisint.io/subdomains/$domain" | jq -Mr .[] | grep -v "no results found" | anew allSubs.txt; done <rootdomains.txt
